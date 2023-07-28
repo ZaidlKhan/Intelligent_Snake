d90e5d5b97753b5e1236111b652bcc6a803d87f5
ZaidlKhan <110517508+ZaidlKhan@users.noreply.github.com>
2023-07-27 17:09:12 -0700

    added AI

--- snake_game.py
+++ snake_game.py
@@ -1,119 +1,217 @@
 import pygame
 import time
 import random
-
-pygame.init()
-
-window_size = (900, 600)
-game_window = pygame.display.set_mode(window_size)
-clock = pygame.time.Clock()
-snake_body = [[100, 50],
-              [90, 50]]
-direction = 'RIGHT'
-score = 0
-fruit_spawn = True
-
-colors = {
-    "black": pygame.Color(0, 0, 0),
-    "white": pygame.Color(255, 255, 255),
-    "red": pygame.Color(255, 0, 0),
-    "green": pygame.Color(0, 255, 0),
-}
-
-snake_head = pygame.transform.scale(pygame.image.load("assets/snake_head.png"), (18, 18))
-apple = pygame.transform.scale(pygame.image.load("assets/carrot.png"), (18, 20))
-carrot = pygame.transform.scale(pygame.image.load("assets/apple.png"), (18, 20))
-melon = pygame.transform.scale(pygame.image.load("assets/melon.png"), (20, 20))
-beet = pygame.transform.scale(pygame.image.load("assets/beet.png"), (20, 20))
-potato = pygame.transform.scale(pygame.image.load("assets/potato.png"), (20, 20))
-
-fruits = [apple, carrot, melon, beet]
+from collections import deque
+from queue import PriorityQueue
 
 
-def random_fruit_position():
-    return [random.randrange(1, window_size[0] // 20) * 20,
-            random.randrange(1, window_size[1] // 20) * 20]
+class SnakeGame:
+    def __init__(self):
+        pygame.init()
 
+        self.cell_size = 20
+        self.grid_size = (30, 30)
+        self.window_size = (self.grid_size[0] * self.cell_size, self.grid_size[1] * self.cell_size)
+        self.game_window = pygame.display.set_mode(self.window_size)
+        self.clock = pygame.time.Clock()
+        self.snake_body = deque([[5, 5], [4, 5]])
+        self.direction = 'RIGHT'
+        self.score = 0
+        self.fruit_spawn = True
+        self.toggle_ai = False
 
-fruit_position = random_fruit_position()
-fruit = random.choice(fruits)
+        self.colors = {
+            "black": pygame.Color(0, 0, 0),
+            "white": pygame.Color(255, 255, 255),
+            "red": pygame.Color(255, 0, 0),
+            "green": pygame.Color(0, 255, 0),
+        }
 
+        self.snake_head = pygame.transform.scale(pygame.image.load("assets/snake_head.png"),
+                                                 (self.cell_size - 1, self.cell_size - 1))
+        apple = pygame.transform.scale(pygame.image.load("assets/carrot.png"), (self.cell_size, self.cell_size))
+        carrot = pygame.transform.scale(pygame.image.load("assets/apple.png"), (self.cell_size, self.cell_size))
+        melon = pygame.transform.scale(pygame.image.load("assets/melon.png"), (self.cell_size, self.cell_size))
+        beet = pygame.transform.scale(pygame.image.load("assets/beet.png"), (self.cell_size, self.cell_size))
 
-def draw_text(content, font, size, color, pos1):
-    font = pygame.font.SysFont(font, size)
-    surface = font.render(content, True, color)
-    rect = surface.get_rect()
-    rect.midtop = pos1
-    game_window.blit(surface, rect)
+        self.fruits = [apple, carrot, melon, beet]
+        self.fruit_position = self.random_fruit_position()
+        self.fruit = random.choice(self.fruits)
 
+    def random_fruit_position(self):
+        while True:
+            new_pos = [random.randrange(0, self.grid_size[0]), random.randrange(0, self.grid_size[1])]
+            if new_pos not in self.snake_body:
+                return new_pos
 
-def game_over():
-    draw_text('Your Score is : ' + str(score), 'times new roman', 50, colors['red'], game_window.get_rect().midtop)
-    pygame.display.flip()
-    time.sleep(2)
-    pygame.quit()
-    quit()
+    def draw_text(self, content, font, size, color, pos1):
+        font = pygame.font.SysFont(font, size)
+        surface = font.render(content, True, color)
+        rect = surface.get_rect()
+        rect.midtop = pos1
+        self.game_window.blit(surface, rect)
 
+    def game_over(self):
+        self.draw_text('Your Score is : ' + str(self.score), 'times new roman', 50, self.colors['red'],
+                       self.game_window.get_rect().midtop)
+        pygame.display.flip()
+        time.sleep(2)
+        pygame.quit()
+        quit()
 
-def draw_rect(color, position):
-    pygame.draw.rect(game_window, color, pygame.Rect(position[0], position[1], 18, 18))
+    def draw_rect(self, color, position):
+        pygame.draw.rect(self.game_window, color,
+                         pygame.Rect(position[0] * self.cell_size,
+                                     position[1] * self.cell_size, self.cell_size - 1, self.cell_size - 1))
 
+    def handle_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if not self.toggle_ai:
+                        self.toggle_ai = True
+                    else:
+                        self.toggle_ai = False
 
-def handle_input():
-    global direction
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and direction != 'DOWN':
-                direction = 'UP'
-            if event.key == pygame.K_DOWN and direction != 'UP':
-                direction = 'DOWN'
-            if event.key == pygame.K_LEFT and direction != 'RIGHT':
-                direction = 'LEFT'
-            if event.key == pygame.K_RIGHT and direction != 'LEFT':
-                direction = 'RIGHT'
+            if not self.toggle_ai:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP and self.direction != 'DOWN':
+                        self.direction = 'UP'
+                    if event.key == pygame.K_DOWN and self.direction != 'UP':
+                        self.direction = 'DOWN'
+                    if event.key == pygame.K_LEFT and self.direction != 'RIGHT':
+                        self.direction = 'LEFT'
+                    if event.key == pygame.K_RIGHT and self.direction != 'LEFT':
+                        self.direction = 'RIGHT'
 
+    def is_collision(self):
+        if self.snake_body[0][0] < 0 or self.snake_body[0][0] > self.grid_size[0] - 1 or self.snake_body[0][1] < 0 \
+                or self.snake_body[0][1] > self.grid_size[1] - 1 or self.snake_body[0] in list(self.snake_body)[1:]:
+            return True
 
-while True:
-    handle_input()
+    def a_star(self, start, goal):
+        def heuristic(a, b):
+            return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
-    if direction == 'UP':
-        snake_body.insert(0, [snake_body[0][0], snake_body[0][1] - 20])
-    if direction == 'DOWN':
-        snake_body.insert(0, [snake_body[0][0], snake_body[0][1] + 20])
-    if direction == 'LEFT':
-        snake_body.insert(0, [snake_body[0][0] - 20, snake_body[0][1]])
-    if direction == 'RIGHT':
-        snake_body.insert(0, [snake_body[0][0] + 20, snake_body[0][1]])
+        frontier = PriorityQueue()
+        frontier.put(start, 0)
+        came_from = {start: None}
+        cost_so_far = {start: 0}
+        snake_body = [tuple(body_part) for body_part in self.snake_body]
 
-    if pygame.Rect(snake_body[0][0], snake_body[0][1], 20, 20).colliderect(
-            pygame.Rect(fruit_position[0], fruit_position[1], 20, 20)):
-        score += 10
-        fruit_spawn = False
-    else:
-        snake_body.pop()
+        while not frontier.empty():
+            current = frontier.get()
 
-    if not fruit_spawn:
-        fruit_position = random_fruit_position()
-        fruit = random.choice(fruits)
-        fruit_spawn = True
+            if current == goal:
+                break
 
-    game_window.fill((40, 40, 40))
-    for index, pos in enumerate(snake_body):
-        if index == 0:
-            game_window.blit(snake_head, pos)
+            for next in [(current[0] + 1, current[1]), (current[0] - 1, current[1]), (current[0], current[1] + 1),
+                         (current[0], current[1] - 1)]:
+                new_cost = cost_so_far[current] + 1
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                    if 0 <= next[0] < self.grid_size[0] and 0 <= next[1] < self.grid_size[1] and next not in snake_body:
+                        cost_so_far[next] = new_cost
+                        priority = new_cost + heuristic(goal, next)
+                        frontier.put(next, priority)
+                        came_from[next] = current
+
+        current = goal
+        path = []
+        while current is not None:
+            path.append(current)
+            current = came_from[current]
+
+        return path[::-1]
+
+    def find_path(self):
+        return self.a_star(tuple(self.snake_body[0]), tuple(self.fruit_position))
+
+    def draw_path(self, path):
+        for i in range(len(path)):
+            pygame.draw.rect(self.game_window, (255, 255, 255),
+                             pygame.Rect(path[i][0] * self.cell_size + 8,
+                                         path[i][1] * self.cell_size + 8, 4, 4))
+
+    def follow_path(self, path):
+        if len(path) < 2:
+            return
+        dx = path[1][0] - path[0][0]
+        dy = path[1][1] - path[0][1]
+        if dx == 1:
+            self.direction = 'RIGHT'
+        elif dx == -1:
+            self.direction = 'LEFT'
+        elif dy == 1:
+            self.direction = 'DOWN'
+        elif dy == -1:
+            self.direction = 'UP'
+
+    def update_snake_position(self):
+        direction_dict = {'UP': [self.snake_body[0][0], self.snake_body[0][1] - 1],
+                          'DOWN': [self.snake_body[0][0], self.snake_body[0][1] + 1],
+                          'LEFT': [self.snake_body[0][0] - 1, self.snake_body[0][1]],
+                          'RIGHT': [self.snake_body[0][0] + 1, self.snake_body[0][1]]}
+        self.snake_body.appendleft(direction_dict[self.direction])
+
+    def handle_fruit_eating(self):
+        if self.snake_body[0] == self.fruit_position:
+            self.score += 10
+            self.fruit_spawn = False
         else:
-            draw_rect(colors["green"], pos)
+            self.snake_body.pop()
 
-    game_window.blit(fruit, fruit_position)
+    def spawn_fruit(self):
+        if not self.fruit_spawn:
+            self.fruit_position = self.random_fruit_position()
+            self.fruit = random.choice(self.fruits)
+            self.fruit_spawn = True
 
-    if snake_body[0][0] < 0 or snake_body[0][0] > window_size[0] - 20 or snake_body[0][1] < 0 \
-            or snake_body[0][1] > window_size[1] - 20 or snake_body[0] in snake_body[1:]:
-        game_over()
+    def draw_game_objects(self):
+        for index, pos in enumerate(self.snake_body):
+            if index == 0:
+                self.game_window.blit(self.snake_head, (pos[0] * self.cell_size, pos[1] * self.cell_size))
+            else:
+                self.draw_rect(self.colors["green"], pos)
 
-    draw_text('Score : ' + str(score), 'times new roman', 20, colors["white"], (40, 0))
-    draw_text('Time : ' + str(pygame.time.get_ticks() // 1000), 'times new roman', 20, colors["white"],
-              (window_size[0] - 60, 0))
-    pygame.display.update()
-    clock.tick(20)
+        self.game_window.blit(self.fruit,
+                              (self.fruit_position[0] * self.cell_size, self.fruit_position[1] * self.cell_size))
+
+    def display_game_info(self):
+        self.draw_text('Score : ' + str(self.score), 'times new roman', 17, self.colors["white"], (40, 0))
+        self.draw_text('Time : ' + str(pygame.time.get_ticks() // 1000), 'times new roman', 17, self.colors["white"],
+                       (self.window_size[0] - 40, 0))
+        if self.toggle_ai:
+            ai = "True"
+        else:
+            ai = "False"
+        self.draw_text("Toggle AI:" + ai, 'times new roman', 17, self.colors["white"],
+                       (self.window_size[0] / 2, 0))
+
+    def play_game(self):
+        while True:
+            self.game_window.fill((40, 40, 40))
+            if self.toggle_ai:
+                path = self.find_path()
+                self.draw_path(path)
+                self.follow_path(path)
+                self.handle_input()
+            else:
+                self.handle_input()
+
+            self.update_snake_position()
+            self.handle_fruit_eating()
+            self.spawn_fruit()
+            self.draw_game_objects()
+
+            if self.is_collision():
+                self.game_over()
+
+            self.display_game_info()
+            pygame.display.update()
+            self.clock.tick(20)
+
+
+if __name__ == "__main__":
+    SnakeGame().play_game()
